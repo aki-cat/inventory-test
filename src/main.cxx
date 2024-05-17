211131cadd1da4f21ca46b19841e182dd17bbb10
aki-cat <orenjiakira@gmail.com>
2024-05-16 21:36:08 -0300

    Code basic hello world

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,17 @@
 #include <raylib.h>
 
 int main() {
+    InitWindow(400, 400, "UITest");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(Color{0x20, 0x20, 0x20, 0xff});
+        if (IsKeyPressed(KeyboardKey::KEY_F8)) {
+            break;
+        }
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
