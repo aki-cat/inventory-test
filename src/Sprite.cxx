8dcd2962541dee7be616f60830411e3d07890ac1
aki-cat <orenjiakira@gmail.com>
2024-05-16 22:24:57 -0300

    Render some assets on the screen as test

---
+++ src/Sprite.cxx
@@ -0,0 +1,19 @@
+//
+// Created by coffeecat on 16/05/24.
+//
+
+#include "Sprite.h"
+
+#include <raylib.h>
+
+Sprite::Sprite(const char *path) : _texture() {
+    _texture = LoadTexture(path);
+}
+
+void Sprite::draw(int x, int y) const {
+    DrawTexture(_texture, x, y, WHITE);
+}
+
+Sprite::~Sprite() {
+    UnloadTexture(_texture);
+}
--- src/main.cxx
+++ src/main.cxx
@@ -1,17 +1,33 @@
+// Main
+
+#include "Sprite.h"
+
 #include <raylib.h>
 
-int main() {
-    InitWindow(400, 400, "UITest");
+void run_loop() {
+    Sprite inventory_window = Sprite("assets/inventory.png");
+    Sprite bucket = Sprite("assets/items/bucket.png");
 
     while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(Color{0x20, 0x20, 0x20, 0xff});
+        // Debug quit
         if (IsKeyPressed(KeyboardKey::KEY_F8)) {
             break;
         }
+
+        // Rendering
+        BeginDrawing();
+
+        ClearBackground(Color{0x20, 0x20, 0x20, 0xff});
+        inventory_window.draw(0, 0);
+        bucket.draw(32, 32);
+
         EndDrawing();
     }
+}
 
+int main() {
+    InitWindow(1280, 720, "UITest");
+    run_loop();
     CloseWindow();
     return 0;
 }
